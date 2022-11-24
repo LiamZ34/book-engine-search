c920a9fd8f983ee6a651685ce1ec2e4a69cb5f2f
Test1 1 <williamh2461@gmail.com>
2022-11-24 12:34:01 -0500

    added const data with bookdata variable to handlesavebook function

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -17,6 +17,8 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [ saveBook, {error} ] = useMutation(SAVE_BOOK);
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -68,11 +70,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { bookData: bookToSave }
+      })
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
