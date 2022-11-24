c48b1822c05d76b4d7400b976c7d328c4e5ea7d7
Test1 1 <williamh2461@gmail.com>
2022-11-24 10:19:46 -0500

    added httpLInk constant along with authLink constant to create the client

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,24 @@ import Navbar from './components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+})
+
 //create client
 const client = new ApolloClient({
-  uri: 'graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
